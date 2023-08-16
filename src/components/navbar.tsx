973f46a66cde8b3741538f6b157a84214fa64a96
Yuto Uogata <113719404+yuto916@users.noreply.github.com>
2023-08-16 17:25:12 -0400

    Added firebase authentication

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,20 @@
 import { Link } from "react-router-dom";
+import { auth } from "../config/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 export const Navbar = () => {
+  const [user] = useAuthState(auth);
+
   return (
-    <div>
-      <Link to="/">Home</Link>
-      <Link to="/login">Login</Link>
+    <div className="navbar">
+     <div>
+        <Link to="/">Home</Link>
+        <Link to="/login">Login</Link>
+     </div>
+      <div>
+        <p> {user?.displayName}</p>
+        <img src={user?.photoURL || ""} />
+      </div>
     </div>
   );
 };
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,4 +1,20 @@
-export const Login = () => {
+import { auth, provider } from "../config/firebase";
+import { signInWithPopup } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
-    return <div>Login Page</div>
-}
+export const Login = () => {
+  const navigate = useNavigate();
+
+  const signInWithGoogle = async () => {
+    const result = await signInWithPopup(auth, provider);
+    console.log(result);
+    navigate("/")
+  };
+
+  return (
+    <div>
+      <p>Sign In With Google To Continue</p>
+      <button onClick={signInWithGoogle}>Sign In With Google</button>
+    </div>
+  );
+};
